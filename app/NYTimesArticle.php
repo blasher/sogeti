90e77b2de31755491b0af4da16f07ff546c38fdc
Brian Lasher <me@brianlasher.com>
2017-06-22 14:59:12 -0500

    addition of inital migration and model for problem 1

---
+++ app/NYTimesArticle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NYTimesArticle extends Model
+{
+    //
+    protected $table   = 'n_y_times_artices';
+    public $timestamps = true;
+}
